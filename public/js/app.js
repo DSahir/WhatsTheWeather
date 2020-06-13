4a552f8af76c42d4659f15fb2c57fe9e504f32a2
Dhanshri <dsahir511@gmail.com>
2020-06-13 04:31:16 -0700

    hosting..

--- public/js/app.js
+++ public/js/app.js
@@ -47,8 +47,8 @@ searchform.addEventListener('submit',(e)=>{
     msgOne.textContent = 'Loading..'
     msgTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
-    //fetch('/weather?address='+location).then((response)=>{
+    //fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 console.log(data.error)
