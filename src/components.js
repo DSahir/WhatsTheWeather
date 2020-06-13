ceb0674e33433fc6ae52aae4c4d744a4ac306812
Dhanshri <dsahir511@gmail.com>
2020-06-13 04:00:53 -0700

    loaction tracking

--- src/components.js
+++ src/components.js
@@ -1,14 +1,18 @@
 const request = require('request')
-const tkn = require('../tokens')
+const accessToken = process.env.AccessTkn || require('../tokens/tokenmapbox')
+const darknetkey = process.env.Darknet  || require('../tokens/tokendarknet')
 
 const geocode = (address , callback )=>{
     //encodeURIComponent() helps encode special characters --mapbox understands it
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?'+tkn.accessToken +'&limit=1'
+    //const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?'+accessToken
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token='+accessToken+'&limit=1'
+    
     request({url : url , json : true}, (error,response)=>{
+        console.log(response.body)
         if(error){
             callback('Unable to connect to location via mapbox' , undefined)
         }else if(response.body.features.length === 0){
-            callback('Unable to find location, try agn! ')
+            callback('Unable to find location, try agn! ' , undefined)
         }else{
             callback(undefined , {
                 latitude : response.body.features[0].center[1]    ,        
@@ -20,7 +24,7 @@ const geocode = (address , callback )=>{
 }
 
 const forcast = (latitude , longitude , callback) =>{
-    const url = 'https://api.darksky.net/forecast/'+tkn.darknetkey +'/'+latitude+','+longitude +'?units=si'
+    const url = 'https://api.darksky.net/forecast/'+darknetkey +'/'+latitude+','+longitude +'?units=si'
      //request({url : url , json :true },(error , response )=>{
      request({url , json :true },(error , {body} )=>{
             if(error){
